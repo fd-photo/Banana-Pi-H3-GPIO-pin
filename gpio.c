b9b390e9d0839863ffaadea83ef9684c92882222
fd-photo <77574164+fd-photo@users.noreply.github.com>
2023-04-10 09:26:22 +0300

    first upload

---
+++ gpio.c
@@ -0,0 +1,57 @@
+#include "gpio.h"  
+ 
+PIO_Map *PIO = NULL;
+unsigned int *gpio_map;
+ 
+void GPIO_Init(void)
+{
+	unsigned int fd;
+	unsigned int addr_start, addr_offset;
+	unsigned int PageSize, PageMask;
+	
+	if((fd = open("/dev/mem",O_RDWR)) == -1)
+	{
+		printf("open error\r\n");
+		return;
+	}
+ 
+	PageSize = sysconf(_SC_PAGESIZE);	// page size
+	PageMask = (~(PageSize-1));			// page mask
+	printf("PageSize:%d,PageMask:%.8X\r\n",PageSize,PageMask);
+ 
+	addr_start = PIO_BASE_ADDRESS & PageMask;
+	addr_offset = PIO_BASE_ADDRESS & ~PageMask;
+	printf("addr_start:%.8X,addr_offset:%.8X\r\n",addr_start,addr_offset);
+ 
+	if((gpio_map = mmap(NULL,PageSize*2,PROT_READ|PROT_WRITE, MAP_SHARED,fd,addr_start)) == NULL)
+	{
+		printf("mmap error\r\n");
+		close(fd);
+		return;
+	}
+//	printf("gpio_map:%.8X\r\n",gpio_map);
+ 
+	PIO = (PIO_Map *)((unsigned int)gpio_map + addr_offset);
+//	printf("PIO:%.8X\r\n",PIO);
+	
+	close(fd);
+}
+ 
+void GPIO_ConfigPin(PORT port,unsigned int pin,PIN_MODE mode)
+{
+    if (gpio_map == NULL)
+        return;
+	PIO->Pn[port].CFG[pin / 8] &= ~((unsigned int)0x07 << pin % 8 * 4);
+	PIO->Pn[port].CFG[pin / 8] |=  ((unsigned int)mode << pin % 8 * 4);
+}
+ 
+void GPIO_SetPin(PORT port,unsigned int pin,unsigned int level)
+{
+    if (gpio_map == NULL)
+        return;
+	if(level)
+		PIO->Pn[port].DAT |= (1 << pin);
+	else
+		PIO->Pn[port].DAT &= ~(1 << pin);
+}
+
---
+++ main2.c
@@ -0,0 +1,40 @@
+#include<stdio.h>  
+#include<stdlib.h>
+#include<unistd.h>  
+#include<sys/mman.h>  
+#include<sys/types.h>  
+#include<sys/stat.h>  
+#include<fcntl.h> 
+#include "gpio.h"  
+
+int main(int argc, const char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("error.\n");
+		exit(1);
+	}
+	
+	long steps     = atoi(argv[1]);    // argv to int
+	int  direction = atoi(argv[2]);    //
+	
+    GPIO_Init();
+
+	GPIO_ConfigPin(PA, 20, OUT);       // 40 banana pi
+	GPIO_ConfigPin(PA, 21, OUT);       // 38 
+    GPIO_ConfigPin(PA, 17, OUT);       // 37
+
+	GPIO_SetPin(PA, 17, 0);            // enable motor
+	GPIO_SetPin(PA, 21, direction);    // direction
+	for (int i=0; i<steps; i++)
+	{
+		GPIO_SetPin(PA,20, 1);
+		usleep(5);
+		GPIO_SetPin(PA,20, 0);
+		usleep(5);
+	}
+	GPIO_SetPin(PA, 17, 1); //disable motor
+	return 0;	
+	//GPIO_Free();
+}
+
